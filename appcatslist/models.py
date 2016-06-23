47bf8ff6dd0d74038cd7fd3751bb177702add246
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-23 14:17:41 -0400

    Profile model created

--- appcatslist/models.py
+++ appcatslist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+cities = (
+        ('greenville', 'Greenville'),
+        ('charleston', 'Charleston'),
+        ('ashville', 'Asheville'),
+        ('atlanta', 'Atlanta')
+        )
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.user')
+    username = models.CharField(max_length=30)
+    preferredcity = models.CharField(max_length=30, choices=cities, null=True)
+    profilepicture = models.ImageField(upload_to='profile_photos', null=True, blank=True, name='profile photo')
