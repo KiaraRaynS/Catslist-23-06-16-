4bd6b7b1b57bc71349d64601d4f5222c46370f25
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-23 14:43:42 -0400

    Posting model created

--- appcatslist/admin.py
+++ appcatslist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from appcatslist.models import UserProfile
 
 # Register your models here.
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['username']
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- appcatslist/models.py
+++ appcatslist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,9 +10,23 @@ cities = (
         ('atlanta', 'Atlanta')
         )
 
+categories = (
+        ('item', 'Item'),
+        ('service', 'Service'),
+        )
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField('auth.user')
     username = models.CharField(max_length=30)
-    preferredcity = models.CharField(max_length=30, choices=cities, null=True)
+    preferredcity = models.CharField(max_length=30, choices=cities, null=True, blank=True)
     profilepicture = models.ImageField(upload_to='profile_photos', null=True, blank=True, name='profile photo')
+
+
+class OfferPost(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.FloatField()
+    category = models.CharField(max_length=20, choices=categories)
+    location = models.CharField(max_length=20, choices=cities)
